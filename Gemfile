dc2cfafcd3c23bc4b4e9286298127a7d45fab972
Alicia Howell <howell33@msu.edu>
2015-02-23 16:28:59 -0500

    project6

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 ruby '1.9.3'
 
 gem 'sinatra', '~> 1.4.5'
+gem 'holidapi'
+gem 'rack', '1.5.2'
--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
+require 'holidapi'
 
 class MyWebApp < Sinatra::Base
 	get '/' do 
-		 erb :"index#{ rand(3) + 1 }"
+
+		@holidays = HolidApi.get(country: 'us', month: Time.new.month, year: Time.new.year)
+		@old_holiday = HolidApi.get(country: 'us', month: 11, year: 1992)
+		erb :index
 	end
 end
