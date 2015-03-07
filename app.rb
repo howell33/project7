6ad373b4bbf245bf3690c235266c8ad099cd117e
Alicia Howell <howell33@msu.edu>
2015-03-06 23:54:27 -0500

    updated app.rb

--- app.rb
+++ app.rb
@@ -3,13 +3,10 @@ require 'holidapi'
 
 class MyWebApp < Sinatra::Base
 	get '/' do 
-
-		params['month'] ||= current_time.month.to_s
-  		params['year'] ||= current_time.year.to_s
-  		params['day'] ||= current_time.day.to_s
-		@current_time = Time.zone.now
+		params['month'] ||= Time.new.month
+		params['day'] ||= Time.new.day
+		params['year'] ||= Time.new.year
 		@holidays = HolidApi.get(country: 'us', month: params['month'], year: params['year'], day: params['day']).flatten
-		@old_holiday = HolidApi.get(country: 'us', month: 11, year: 1992).flatten
 		erb :index
 	end
 end
